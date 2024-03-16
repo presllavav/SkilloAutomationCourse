7e67433a3bca363b8104da6d102620a3584e2fdc
Preslava <preslavapetrovaa3@gmail.com>
2024-03-16 20:48:53 +0200

    Code 1

---
+++ src/main/java/Homework2/lesson8/Shape.java
@@ -0,0 +1,24 @@
+package main.java.Homework2.lesson8;
+
+abstract class Shape {
+    public abstract void getArea();
+    public  void getPerimeter(){
+
+    }
+    class Circle extends Shape {
+        public void getArea() {
+
+        }
+        public void getPerimeter(){
+
+        }
+    }
+    class Square extends Shape {
+        public void getArea() {
+            System.out.println();
+        }
+        public void getPerimeter(){
+
+        }
+    }
+}
---
+++ src/main/java/lesson6/Even.java
@@ -0,0 +1,15 @@
+package main.java.lesson6;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+
+        System.out.println("Write the number:");
+
+        Scanner s =  new Scanner(System.in);
+        int number = Integer.parseInt(s.nextLine());
+
+
+    }
+}
